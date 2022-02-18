f84713c1ef623bdf1522298917bb46188fd09297
Al Newkirk <al@iamalnewkirk.com>
2022-02-18 12:05:03 -0500

    Implement Venus::Kind#{safe,trap} (#119)

--- lib/Venus/Kind.pm
+++ lib/Venus/Kind.pm
@@ -24,12 +24,48 @@ sub class {
   return ref($self) || $self;
 }
 
+sub safe {
+  my ($self, $method, @args) = @_;
+
+  my $result = $self->trap($method, @args);
+
+  return(wantarray ? (@$result) : $result->[0]);
+}
+
 sub space {
   my ($self) = @_;
 
   require Venus::Space;
 
   return Venus::Space->new($self->class);
+}
+
+sub trap {
+  my ($self, $method, @args) = @_;
+
+  no strict;
+  no warnings;
+
+  my $result = [[],[],[]];
+
+  return(wantarray ? (@$result) : $result->[0]) if !$method;
+
+  local ($!, $?, $@, $^E);
+
+  local $SIG{__DIE__} = sub{
+    push @{$result->[2]}, @_;
+  };
+
+  local $SIG{__WARN__} = sub{
+    push @{$result->[1]}, @_;
+  };
+
+  push @{$result->[0]}, eval {
+    local $_ = $self;
+    $self->$method(@args);
+  };
+
+  return(wantarray ? (@$result) : $result->[0]);
 }
 
 sub type {
